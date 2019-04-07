e166787e6102e5ab61f66bb44ed13b3a1399af0a
Tobias Trinks <tobias.trinks99@gmx.net>
2019-04-07 17:05:03 +0200

    Feat add endpoint to update job status

---
+++ src/test/kotlin/com/hostiflix/service/JobServiceTest.kt
@@ -0,0 +1,54 @@
+package com.hostiflix.service
+
+import com.hostiflix.entity.JobStatus
+import com.hostiflix.repository.JobRepository
+import com.hostiflix.support.MockData
+import com.nhaarman.mockito_kotlin.given
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
+import java.util.*
+
+@RunWith(MockitoJUnitRunner::class)
+class JobServiceTest {
+
+    @Mock
+    private lateinit var jobRepository: JobRepository
+
+    @InjectMocks
+    private lateinit var jobService: JobService
+
+    @Test
+    fun `should return null if job id is not existing`() {
+        // given
+        val id = "invalid-id"
+        given(jobRepository.findById(id)).willReturn(Optional.empty())
+
+        // when
+        val result = jobService.updateJobStatusById(id, JobStatus.BUILD_FAILED)
+
+        // then
+        assertThat(result).isNull()
+    }
+
+    @Test
+    fun `should return job after job was update successfully`() {
+        // given
+        val id = "1234"
+        val newStatus = JobStatus.DEPLOYMENT_SUCCESSFUL
+        val job = MockData.job("j1", MockData.branch("b1", MockData.project("p1", "c1")))
+
+        given(jobRepository.findById(id)).willReturn(Optional.of(job))
+        given(jobRepository.save(job)).willReturn(job)
+
+        // when
+        val result = jobService.updateJobStatusById(id, newStatus)
+
+        // then
+        assertThat(result).isNotNull
+        assertThat(result!!.status).isEqualTo(newStatus)
+    }
+}
