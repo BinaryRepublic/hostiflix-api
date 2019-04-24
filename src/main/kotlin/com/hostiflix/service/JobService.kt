073ccb7237e214a2fc6568cfc3fe9f5fe18f48f0
Tobias Kemkes <kemkes-tobias@web.de>
2019-04-24 17:05:00 +0200

    Fixed tests

--- src/main/kotlin/com/hostiflix/service/JobService.kt
+++ src/main/kotlin/com/hostiflix/service/JobService.kt
@@ -4,6 +4,7 @@ import com.hostiflix.entity.Job
 import com.hostiflix.entity.JobStatus
 import com.hostiflix.repository.JobRepository
 import org.springframework.stereotype.Service
+import java.time.Instant
 
 @Service
 class JobService (
@@ -12,6 +13,9 @@ class JobService (
     fun updateJobStatusById(id: String, status: JobStatus): Job? {
         val job = jobRepository.findById(id).takeIf { it.isPresent }?.get() ?: return null
         job.status = status
+        if (status == JobStatus.DEPLOYMENT_SUCCESSFUL || status == JobStatus.DEPLOYMENT_FAILED || status == JobStatus.BUILD_FAILED) {
+            job.finishedAt = Instant.now()
+        }
         return jobRepository.save(job)
     }
 }
